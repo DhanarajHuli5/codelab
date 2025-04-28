5bbe177d1598890a699a41e406fa88abd3f8b80f
DhanarajHuli5 <dhanarajhuli5@gmail.com>
2025-04-29 04:10:05 +0530

    Authentication

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Hello from code lab");
+});
 
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`);
 });
